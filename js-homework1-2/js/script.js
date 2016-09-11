1083c68152978efc27ee49d9662143a2d41dd518
jamess <ukr.jmss@gmail.com>
2016-09-11 15:21:26 +0200

    js1-2

--- js-homework1-2/js/script.js
+++ js-homework1-2/js/script.js
@@ -12,3 +12,25 @@ function myPow(myNumber, myMultiply) {
   }
   console.log(result);
 }
+
+alert('Задача 2');
+
+var myArray = [];
+
+for (var i = 0; i < 5; i++) {
+  myArray[myArray.length] = prompt('Enter a name');
+}
+
+console.log(myArray);
+
+var userName = prompt('Enter a username');
+
+for(var i = 0; i < myArray.length; i++) {
+  if(userName !== myArray[i]) {
+    alert('Ошибка');
+    break;
+  } else {
+    alert('Вы успешно вошли ' + userName);
+    break;
+  }
+}
